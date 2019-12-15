1f588cdb62f07da086c2448a028e9fbddebcdc52
Nico Krieg <n.krieg123@googlemail.com>
2019-12-15 18:38:30 +0100

    Change SSL configuration of Spring RestTemplate to trust all self-signed certificates.

--- java-samples/exercise-a2/src/main/java/de/tls4developers/examples/exercisea2/HttpsEnabledRestTemplateConfiguration.java
+++ java-samples/exercise-a2/src/main/java/de/tls4developers/examples/exercisea2/HttpsEnabledRestTemplateConfiguration.java
@@ -2,12 +2,11 @@ package de.tls4developers.examples.exercisea2;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContextBuilder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,24 +15,17 @@ import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 
 @Configuration
 public class HttpsEnabledRestTemplateConfiguration {
 
-    @Value("${http.client.ssl.trust-store}")
-    private Resource keyStoreResource;
-
-    @Value("${http.client.ssl.trust-store-password}")
-    private String keyStorePassword;
-
     @Bean
     public RestTemplate restTemplate() throws IOException, CertificateException, NoSuchAlgorithmException,
-            KeyStoreException, KeyManagementException, UnrecoverableKeyException {
+            KeyStoreException, KeyManagementException {
 
         SSLContext sslContext = new SSLContextBuilder()
-                .loadTrustMaterial(keyStoreResource.getFile(), keyStorePassword.toCharArray())
+                .loadTrustMaterial(null, new TrustSelfSignedStrategy())
                 .build();
         SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(sslContext);
         HttpClient httpClient = HttpClients
